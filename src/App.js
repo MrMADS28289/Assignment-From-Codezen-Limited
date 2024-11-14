789e7278422542dd4d80e4213cacf4eeba930df5
MrMADS28289 <abduss.sobhan28@gmail.com>
2024-11-14 16:24:38 +0600

    vercel error solved

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import TabsComponent from "./Components/TabsComponent";
-import Header from "./Components/Header";
 import "./App.css";
-import DesignSystem from "./Components/DesignSystem";
 import Sidebar from "./Components/Sidebar";
 import MainContent from "./Components/MainContent";
 import { useState } from "react";
